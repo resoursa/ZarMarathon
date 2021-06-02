0f8a6f0b6a4c34a48b84db69bdb6ac5514361b2b
bulsond <bulsond@gmail.com>
2021-06-02 19:23:39 +0300

    Task #1-3 создание игрока и отображение в DOM

--- js/main.js
+++ js/main.js
@@ -23,3 +23,43 @@ const player2 = {
 console.log(player1.attack());
 console.log(player2);
 console.log(player2.attack()); */
+
+// вспомогательные функции для создания игрока в DOM
+const buildElement = tag => attr => func => func(tag, attr);
+const withClassName = (tag, className) => {
+    const $el = document.createElement(tag);
+    $el.classList.add(className);
+    return $el;
+};
+const withSrc = (tag, src) => {
+    const $el = document.createElement(tag);
+    $el.src = src;
+    return $el;
+};
+const getDiv = buildElement('div');
+const getImg = buildElement('img');
+
+// функция создания игрока в DOM
+function createPlayer(playerId, playerObj) {
+    const $player = getDiv(playerId)(withClassName);
+    const $progressbar = getDiv('progressbar')(withClassName);
+    const $life = getDiv('life')(withClassName);
+    const $name = getDiv('name')(withClassName);
+    const $character = getDiv('character')(withClassName);
+    const $img = getImg(playerObj.img)(withSrc);
+
+    $life.innerText = playerObj.hp + '%';
+    $name.innerText = playerObj.name;
+
+    $progressbar.appendChild($life);
+    $progressbar.appendChild($name);
+    $character.appendChild($img);
+    $player.appendChild($progressbar);
+    $player.appendChild($character);
+
+    const $root = document.querySelector('.arenas');
+    $root.appendChild($player);
+}
+
+createPlayer('player1', player1);
+createPlayer('player2', player2);
